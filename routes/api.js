fd82c73d4763b0a7b3f7ed0e0cc946cb5039d413
rowanh22 <rowanhart22@gmail.com>
2020-04-02 12:57:32 +1300

    Added basic login/me api endpoints

--- routes/api.js
+++ routes/api.js
@@ -18,31 +18,35 @@ export default router => {
 
     });
 
-    router.get("/login", (req, res) => {
-        var authorizeURL = spotifyApi.createAuthorizeURL(scopes);
-        res.send(authorizeURL);
+    router.get("/me", (req, res) => {
+        spotifyApi.getMe()
+        .then(function(data) {
+            console.log('Some information about the authenticated user', data.body);
+            res.json(data);
+        }, function(err) {
+            console.log('Something went wrong! (me)', err);
+            res.status(400).end();
+        });
     });
 
-    router.post("/getLoginCode", (req, res) => {
-        console.log("QS IS: " + req.queryString);
-        const queryString = req.queryString;
-        const urlParams = new URLSearchParams(queryString);
-        const code = urlParams.get('code');
-        // Retrieve an access token and a refresh token
-        console.log('CODE IS: ' + code);
-        spotifyApi.authorizationCodeGrant(code).then(
-        function(data) {
-            console.log('The token expires in ' + data.body['expires_in']);
-            console.log('The access token is ' + data.body['access_token']);
-            console.log('The refresh token is ' + data.body['refresh_token']);
-
-            // Set the access token on the API object to use it in later calls
-            spotifyApi.setAccessToken(data.body['access_token']);
-            spotifyApi.setRefreshToken(data.body['refresh_token']);
-        },
-        function(err) {
-            console.log('Something went wrong!', err);
-        }
-        ); 
+    router.post("/login", (req, res) => {
+        spotifyApi.authorizationCodeGrant(req.body.code).then(
+            function(data) {
+                console.log("Successfully retrieved tokens!");
+                console.log('The token expires in ' + data.body['expires_in']);
+                console.log('The access token is ' + data.body['access_token']);
+                console.log('The refresh token is ' + data.body['refresh_token']);
+    
+                // Set the access token on the API object to use it in later calls
+                spotifyApi.setAccessToken(data.body['access_token']);
+                spotifyApi.setRefreshToken(data.body['refresh_token']);
+                res.json(data);
+            },
+            function(err) {
+                console.log('Something went wrong!', err);
+                res.status(400).end();
+            });
     });
+
+
 }
--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import { clientId, redirectUri, scopes } from "./config";
-//import hash from "./hash";
 import logo from "./logo.svg";
 import "./App.css";
 var SpotifyWebApi = require('spotify-web-api-node');
 
-//Create spotify api
+//Create spotify api (needed to create authorization URL)
 var spotifyApi = new SpotifyWebApi({
   clientId: clientId,
   clientSecret: '3fbb80a0fa384eab8e096c6a96582cc6',
@@ -13,64 +12,49 @@ var spotifyApi = new SpotifyWebApi({
 });
 // Create the authorization URL
 var authorizeURL = spotifyApi.createAuthorizeURL(scopes) + "&show_dialog=true";
-//console.log(authorizeURL);
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { loggedIn : "",
-                   accessToken : "" };
+    this.state = { loggedIn : "" };
     this.callLogin = this.callLogin.bind(this);
   }
 
-  // getLoginCode() {
-  //   const queryString = window.location.search;
-  //   const urlParams = new URLSearchParams(queryString);
-  //   const code = urlParams.get('code');
-  //   console.log("CODE IS: " + code);
-  //   this.setState({loggedIn : code});
-  // }
-
+  //Called on click of login button
   callLogin() {
     var popup = window.open(
       authorizeURL,
       'Login with Spotify',
       'width=600,height=800'
     )
+    //Create callback function to be called 
     window.spotifyCallback = (payload) => { 
+      //Close popup and tell server to log in
       popup.close();
-      console.log(payload);
+      fetch("/api/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            code: payload
+        })
+      })
+      .then(
+        res => res.json())
+        .then(json => console.log(json));
     }
   }
 
   componentDidMount() {
+    //Retrieve auth code from URL
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const code = urlParams.get('code');
-    console.log("TOKEN IS: " + code);
+    //If code exists i.e if this component is within the popup, call the previous callback function to close popup
     if (code) {
       window.opener.spotifyCallback(code);
     }
-    //Get code
-    //const queryString = window.location.search;
-    //const urlParams = new URLSearchParams(queryString);
-    //const code = urlParams.get('code');
-    // Retrieve an access token and a refresh token
-    //console.log('CODE IS: ' + code);
-    // spotifyApi.authorizationCodeGrant(code).then(
-    //   function(data) {
-    //     console.log('The token expires in ' + data.body['expires_in']);
-    //     console.log('The access token is ' + data.body['access_token']);
-    //     console.log('The refresh token is ' + data.body['refresh_token']);
-
-    //     // Set the access token on the API object to use it in later calls
-    //     spotifyApi.setAccessToken(data.body['access_token']);
-    //     spotifyApi.setRefreshToken(data.body['refresh_token']);
-    //   },
-    //   function(err) {
-    //     console.log('Something went wrong!', err);
-    //   }
-    // );
   }
 
   render() {
