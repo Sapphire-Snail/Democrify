f834254b954c3b8955c6ebc37d5f847514732de8
rowanh22 <rowanhart22@gmail.com>
2020-04-06 09:09:50 +1200

    Added persist store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import { Provider } from 'react-redux';
+import Loader from "react-loader-spinner";
 import App from './App';
 //import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={<Loader type="ThreeDots" color="#1ECD97" height={100} width={100} />} persistor={persistor}>
+            <App/>
+        </PersistGate>
     </Provider>, document.querySelector('#root')
 );
 // If you want your app to work offline and load faster, you can change
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
-//import { composeWithDevTools } from 'redux-devtools-extension';
 
-//import { persistStore, persistReducer } from 'redux-persist';
-//import storage from 'redux-persist/lib/storage';
-//import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 import thunk from 'redux-thunk';
 
@@ -14,23 +13,23 @@ const initialState = {
     user: {}
 }
 
-// Config for redux-persist
-// const persistConfig = {
-//     key: "root",
-//     storage: storage,
-//     stateReconciler: autoMergeLevel2
-// };
-// const pReducer = persistReducer(persistConfig, rootReducer);
+//Config for redux-persist
+const persistConfig = {
+    key: "root",
+    storage: storage,
+    stateReconciler: autoMergeLevel2
+};
+const pReducer = persistReducer(persistConfig, rootReducer);
 
-// Create the store, where...
+// Create the store
 export const store = createStore(
-    rootReducer, // This is our reducer (currently the redux-persist one)...
-    initialState, // This is our initial state...
-    compose( // These are the middlewares we're applying:
-        applyMiddleware(thunk), // redux-thunk, and...
-        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f // The devttools
+    pReducer,
+    initialState,
+    compose( // Middlewares we're applying:
+        applyMiddleware(thunk), // redux-thunk
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f // Devttools
     )
 );
 
 // Enable persistence of the store
-//export const persistor = persistStore(store);
+export const persistor = persistStore(store);
