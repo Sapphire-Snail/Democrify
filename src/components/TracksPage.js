3ec7c250ff95003e1f1b2b0709974e4f2daa85db
Daria <dder725@aucklanduni.ac.nz>
2020-05-14 20:39:50 +1200

    Add a sticky container for a playlist name and button

--- src/components/TracksPage.js
+++ src/components/TracksPage.js
@@ -1,55 +1,70 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getPlaylistTracks } from '../redux/actions/thunk';
-import Tracks from './Tracks';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getPlaylistTracks } from "../redux/actions/thunk";
+import Tracks from "./Tracks";
+import { Link } from "react-router-dom";
 import * as spotify from "../SpotifyFunctions.js";
-import SearchPage from './SearchPage';
-import { Button } from 'reactstrap';
+import SearchPage from "./SearchPage";
+import { Button } from "reactstrap";
+import "./TrackPage.css";
 
-class TracksPage extends Component  {
+class TracksPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchShowing: false,
+    };
+    this.toggleSearch = this.toggleSearch.bind(this);
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            searchShowing: false
-        }
-        this.toggleSearch = this.toggleSearch.bind(this);
+  toggleSearch() {
+    this.setState({ searchShowing: !this.state.searchShowing });
+    if (this.state.searchShowing) {
+      this.componentDidMount();
     }
+  }
 
-    toggleSearch() {
-        this.setState({searchShowing:!this.state.searchShowing});
-        if(this.state.searchShowing) {
-            this.componentDidMount();
-        }
-    }
-    
-    componentDidMount() {
-        this.props.getPlaylistTracks(this.props.playlistId);
-    }
-    
-    render() {
-        return (
-            <div>
-                <Link to='/playlists'>Back we go bois</Link>
-                <div style={{textAlign:"right", marginRight : 10, marginBottom : 10}}><Button style={{backgroundColor: "#c030ed", borderColor : "#c030ed"}} onClick={this.toggleSearch}>{this.state.searchShowing ? 'Back' : 'Add track'}</Button></div>
-                {this.state.searchShowing ? <SearchPage/> : <Tracks title={this.props.activePlaylist.name} col1Name="Name" col2Name="Artist"/>}
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.props.getPlaylistTracks(this.props.playlistId);
+  }
+
+  render() {
+    return (
+      <div>
+        <Link to="/playlists">Back we go bois</Link>
+        <div className="stickyContainer">
+        <h1 className="playlistTitle">{this.props.activePlaylist.name}</h1>
+          <Button  className="addButton"
+            style={{ backgroundColor: "#c030ed", borderColor: "#c030ed" }}
+            onClick={this.toggleSearch}
+          >
+            {this.state.searchShowing ? "Back" : "Add track"}
+          </Button>
+        </div>
+        {this.state.searchShowing ? (
+          <SearchPage />
+        ) : (
+          <Tracks
+            col1Name="Name"
+            col2Name="Artist"
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 //State is entire state tree
 function mapStateToProps(state) {
-    return {
-        activePlaylist: state.playlists.active_playlist,
-        playlistId: window.location.pathname.split('/').pop(), //Grab playlist ID from URL
-        deviceId: state.webplayer.deviceId
-    };
+  return {
+    activePlaylist: state.playlists.active_playlist,
+    playlistId: window.location.pathname.split("/").pop(), //Grab playlist ID from URL
+    deviceId: state.webplayer.deviceId,
+  };
 }
 
 const mapDispatchToProps = {
-    getPlaylistTracks,
-}
+  getPlaylistTracks,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(TracksPage);
