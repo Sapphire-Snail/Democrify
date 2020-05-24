7276cde1722bd6a85d5fa1456668f51a13a90671
rowanh22 <rowanhart22@gmail.com>
2020-05-24 17:31:57 +1200

    Styled error page

--- src/index.js
+++ src/index.js
@@ -8,25 +8,49 @@ import { store, persistor } from "./redux/store";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Notifications from "react-notify-toast";
-
+import { Button } from "reactstrap";
+import { Redirect } from "react-router-dom";
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { 
+      hasError: false,
+      isRedirect: false
+     };
   }
 
   componentDidCatch(error, info) {
     // Display fallback UI
     this.setState({ hasError: true });
-    // You can also log the error to an error reporting service
     console.log(error, info);
   }
 
+  redirect = () => {
+    this.setState({ isRedirect: true})
+  }
+
   render() {
+    if(this.state.isRedirect) {
+      return <Redirect to="/login" />
+    }
     if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return (
+        <div style={{textAlign: 'center'}}>
+          <header>
+            <img
+              className="App-logo"
+              src={require("./assets/logo.svg")}
+              alt="logo"
+            />
+            <h2 className="slogan">Music for the people</h2>
+          </header>
+          <div>
+            <h1>Something went wrong :(</h1>
+            <Button href='/login' color="primary">Back to safety!</Button>
+          </div>
+        </div>
+      );
     }
     return this.props.children;
   }
