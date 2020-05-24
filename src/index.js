30ea81f6cf6c94247b135888a54bab216c6fd7d9
rowanh22 <rowanhart22@gmail.com>
2020-05-24 16:47:17 +1200

    Added some basic error handling and 404 page

--- src/index.js
+++ src/index.js
@@ -7,7 +7,30 @@ import Loader from "react-loader-spinner";
 import { store, persistor } from "./redux/store";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Notifications from 'react-notify-toast';
+import Notifications from "react-notify-toast";
+
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    // Display fallback UI
+    this.setState({ hasError: true });
+    // You can also log the error to an error reporting service
+    console.log(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
 
 ReactDOM.render(
   <Provider store={store}>
@@ -17,13 +40,11 @@ ReactDOM.render(
       }
       persistor={persistor}
     >
-      <Notifications/>
-      <App />
+      <Notifications />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </PersistGate>
   </Provider>,
   document.querySelector("#root")
 );
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
