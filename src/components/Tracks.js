d1f7cbda643bfa103ff139b74e439da79096c0a5
rowanh22 <rowanhart22@gmail.com>
2020-05-24 18:28:15 +1200

    Merge

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Loader from "react-loader-spinner";;
+import Loader from "react-loader-spinner";
 import SingleTrack from "./SingleTrack";
 import { getPlaylistTracks, getPlaylistTracksFromSpotifyAndDB, addSongsFromDBToSpotifyThenGetTracks } from "../redux/actions/thunk";
 import { Table } from "reactstrap";
